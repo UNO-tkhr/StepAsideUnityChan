1ed0e14effafd6d3008f5ee44864d089a51db27a
UNO <uno.tkhr@gmail.com>
2017-07-17 22:56:54 +0900

    課題実装(発展課題未実施)

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+
+	private GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		this.mainCamera = GameObject.Find("Main Camera");
+		//回転を開始する角度を設定
+		this.transform.Rotate(0, Random.Range(0,360), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.z < mainCamera.transform.position.z){
+			Destroy(this.gameObject);
+		}
+		//回転
+		this.transform.Rotate(0,3,0);
+	}
+}
